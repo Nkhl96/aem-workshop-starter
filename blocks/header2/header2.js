15c76483ddd50bbee026ae5506fa1612f1c1cc88
Nikhil Lakshmisha <nikhil.lakshmisha@cognizant.com>
2026-06-10 13:46:54 +1000

    Removed remote content

--- blocks/header2/header2.js
+++ blocks/header2/header2.js
@@ -17,34 +17,34 @@ export default async function decorate(block) {
   const data = block.dataset;
 
   // eslint-disable-next-line no-console
-  console.log('HeaderMenu2 data attributes:', data); // Debug log to check data attributes
+  console.log('HeaderMenu2 data attributes:', data, component); // Debug log to check data attributes
 
-  // Set variation with default
-  component.variation = data.variation || 'transparent';
+  // // Set variation with default
+  // component.variation = data.variation || 'transparent';
 
-  // Load menu items from data or use mock data
-  if (data.menuItems) {
-    component.menuItems = JSON.parse(data.menuItems);
-  } else {
-    // component.menuItems = mockMenuItems;
-  }
+  // // Load menu items from data or use mock data
+  // if (data.menuItems) {
+  //   component.menuItems = JSON.parse(data.menuItems);
+  // } else {
+  //   // component.menuItems = mockMenuItems;
+  // }
 
-  // Load bookmarks link from data or use mock data
-  if (data.bookmarksLink) {
-    component.bookmarksLink = JSON.parse(data.bookmarksLink);
-  } else {
-    // component.bookmarksLink = mockBookmarks;
-  }
+  // // Load bookmarks link from data or use mock data
+  // if (data.bookmarksLink) {
+  //   component.bookmarksLink = JSON.parse(data.bookmarksLink);
+  // } else {
+  //   // component.bookmarksLink = mockBookmarks;
+  // }
 
-  // Set search results page with default
-  component.searchResultsPage = data.searchResultsPage || '/search';
+  // // Set search results page with default
+  // component.searchResultsPage = data.searchResultsPage || '/search';
 
-  // Load popular search terms from data or use mock data
-  if (data.popularSearchTerms) {
-    component.popularSearchTerms = JSON.parse(data.popularSearchTerms);
-  } else {
-    // component.popularSearchTerms = mockSearchTerms;
-  }
+  // // Load popular search terms from data or use mock data
+  // if (data.popularSearchTerms) {
+  //   component.popularSearchTerms = JSON.parse(data.popularSearchTerms);
+  // } else {
+  //   // component.popularSearchTerms = mockSearchTerms;
+  // }
 
   block.replaceChildren(component);
 
